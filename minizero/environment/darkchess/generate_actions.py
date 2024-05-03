bf3d7e1d965ce34eb5bd63314abf48b1145d1dcb
JHTNT <jerry1106666@gmail.com>
2024-05-03 18:25:12 +0800

    feat: change action format

--- minizero/environment/darkchess/generate_actions.py
+++ minizero/environment/darkchess/generate_actions.py
@@ -1,23 +1,22 @@
 board = [
-    ["a8", "b8", "c8", "d8"],
-    ["a7", "b7", "c7", "d7"],
-    ["a6", "b6", "c6", "d6"],
-    ["a5", "b5", "c5", "d5"],
-    ["a4", "b4", "c4", "d4"],
-    ["a3", "b3", "c3", "d3"],
-    ["a2", "b2", "c2", "d2"],
-    ["a1", "b1", "c1", "d1"],
+    [28, 29, 30, 31],
+    [24, 25, 26, 27],
+    [20, 21, 22, 23],
+    [16, 17, 18, 19],
+    [12, 13, 14, 15],
+    [8, 9, 10, 11],
+    [4, 5, 6, 7],
+    [0, 1, 2, 3],
 ]
 
 cnt = 0
 for i in range(8):
     for j in range(4):
-        print(f"{board[i][j]} {board[i][j]}")
+        print(f"{{{board[i][j]}, {board[i][j]}}}, ", end="")
         for k in range(8):
             if k != i:
-                print(f"{board[i][j]} {board[k][j]}")
+                print(f"{{{board[i][j]}, {board[k][j]}}}, ", end="")
         for k in range(4):
             if k != j:
-                print(f"{board[i][j]} {board[i][k]}")
-
-
+                print(f"{{{board[i][j]}, {board[i][k]}}}, ", end="")
+        print()
